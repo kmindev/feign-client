3a6f52afa1d61469d6fe55ecd4b3d664245e53ee
kmindev <kangkm0324@gmail.com>
2024-08-26 18:17:00 +0900

    예제를 위한 외부 컴포넌트 역할을 하는 컨트롤러

---
+++ src/main/java/dev_be/feign_client/controller/TargetController.java
@@ -0,0 +1,19 @@
+package dev_be.feign_client.controller;
+
+import dev_be.feign_client.feign.common.dto.BaseResponseInfo;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/target_server")
+@RestController
+public class TargetController {
+
+    @GetMapping("/get")
+    public BaseResponseInfo demoGet(
+            @RequestHeader("CustomHeaderName") String header,
+            @RequestParam("name") String name,
+            @RequestParam("age") Long age
+    ) {
+        return BaseResponseInfo.of(header, name, age);
+    }
+
+}
---
+++ src/main/java/dev_be/feign_client/feign/common/dto/BaseResponseInfo.java
@@ -0,0 +1,14 @@
+package dev_be.feign_client.feign.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record BaseResponseInfo(
+        String name,
+        Long age,
+        String header
+) {
+    public static BaseResponseInfo of(String header, String name, Long age) {
+        return new BaseResponseInfo(name, age, header);
+    }
+}
